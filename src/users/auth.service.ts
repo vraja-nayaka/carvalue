f0584a69d69fd8f4107aaa9b573b9bba344ad5a9
Vadim Tokar <tokar.web@biganto.com>
2022-03-28 20:44:11 +0300

    feature: signin

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { randomBytes, scrypt as _scrypt } from 'crypto';
 import { promisify } from 'util';
 import { UsersService } from './users.service';
@@ -29,7 +29,19 @@ export class AuthService {
     return user;
   }
 
-  signin() {
+  async signin(email: string, password: string) {
+    const [user] = await this.usersService.find(email);
+    if (!user) {
+        throw new NotFoundException('user not found');
+    }
+    const [salt, storedHash] = user.password.split('.');
 
+    const hash = (await scrypt(password, salt, 32))as Buffer;
+
+    if (storedHash !== hash.toString('hex')) {
+        throw new BadRequestException('bad password');
+    }
+
+    return user;
   }
 }
