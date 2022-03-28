a2c386244c928384cf5abdb40da4db2edf658429
Vadim Tokar <tokar.web@biganto.com>
2022-03-28 19:28:22 +0300

    feature: add auth service

---
+++ src/users/auth.service.ts
@@ -0,0 +1,21 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async signup(email: string, password: string) {
+    const user = await this.usersService.find(email);
+    if (user.length) {
+        throw new BadRequestException('email in use');
+    }
+
+    // Hash the user password
+
+  }
+
+  signin() {
+
+  }
+}
