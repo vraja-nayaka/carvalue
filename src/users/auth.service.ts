70c7636ad63f62807a587e73533e0595c9701295
Vadim Tokar <tokar.web@biganto.com>
2022-03-28 20:31:20 +0300

    feature: add signup with hashed password

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,18 +1,32 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
 import { UsersService } from './users.service';
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
 
   async signup(email: string, password: string) {
-    const user = await this.usersService.find(email);
-    if (user.length) {
+    const users = await this.usersService.find(email);
+    if (users.length) {
         throw new BadRequestException('email in use');
     }
 
     // Hash the user password
+    // Generate a salt
+    const salt = randomBytes(8).toString('hex');
 
+    // Hash the salt and the password together
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    const result = salt + '.' + hash.toString('hex');
+
+    const user = this.usersService.create(email, result);
+
+    return user;
   }
 
   signin() {
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,6 +10,7 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { Serialize } from 'src/interseptors/serialize.interseptor';
+import { AuthService } from './auth.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserDto } from './dtos/user.dto';
@@ -18,11 +19,14 @@ import { UsersService } from './users.service';
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+  ) {}
 
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    this.usersService.create(body.email, body.password);
+    return this.authService.signup(body.email, body.password);
   }
 
   @Get('/:id')
